90d9c7ff279e636d9bd5b16b82d16813c540ee71
Arman Anaturk <armananaturk@gmail.com>
2015-05-13 10:49:58 +0100

    Add validations for Users ad Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,11 +1,16 @@
 class Listing < ActiveRecord::Base
-	if Rails.env.development?
+	 if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-else
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-	  				:storage => :dropbox,
-      				:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                    :path => ":style/:id_:filename"
+  else
+    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ":style/:id_:filename"
+  end
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-end
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates_attachment_presence :image
 end
